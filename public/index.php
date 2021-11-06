0b0fd388c2dd54354af652e00f702899b97d3a00
Lepatron973 <djod973@live.fr>
2021-11-06 05:17:00 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/glide.core.min.css">
+    <link rel="stylesheet" href="css/glide.theme.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php include "header.html"; ?>
+    <main>
+        <?php include "add-movie.html"; ?>
+    </main>
+    <!-- Start Footer -->
+    <?php include "footer.html"; ?>
+    <!-- End Footer -->
+    <script src="js/glide.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+var glide = new Glide('.glide', {
+  type: 'carousel',
+  perView: 3,
+  focusAt: 'center',
+//   breakpoints: {
+//     800: {
+//       perView: 2
+//     },
+//     480: {
+//       perView: 1
+//     }
+//   }
+})
+
+glide.mount()
