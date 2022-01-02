77720d5500910b9e9d8dff3daf870a9360ba3729
Lepatron973 <djod973@live.fr>
2022-01-02 19:56:58 +0100

    ajout de la fonctionnalité de trie sur la page d'accueil

--- src/app/AppHome.js
+++ src/app/AppHome.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Banner1 from '../components/Banner1';
 import Block from '../components/Block';
 import Movie from '../components/Movie';
 import Slider from '../components/Slider';
+import { host,getMovieEndpoint,language,API_KEY } from '../utilities';
 
 const AppHome = () => {
+    const sort = (e) => {
+        setsorted(e.target.value)
+        
+    }
+    let index = 0;
+    const [sorted, setsorted] = useState("popular")
+    const [pageNumber, setPageNumber] = useState(1)
+    const [maxPages, setMaxPages] = useState(0);
+    const [maxResult, setMaxResult] = useState(0);
+    const [limit,setLimit] = useState(10);
+    let pages;
     return (
         <>
             
             <Block blockNumber="1" customClass="" children={[<ElementB1 />]}/>
             <Block blockNumber="2" customClass="" children={
                 [
-                    <ElementB2 title="Les plus populaires" />,
-                    <Movie />
+                    <ElementB2 title="Les plus populaires" action={sort} limit={limit} maxResult={maxResult} 
+                        setLimit={(e)=>{ if(e.target.value<= maxResult) setLimit(e.target.value) }}
+                        pageNumber={pageNumber} maxPages={ pages = maxPages <= 500 ? maxPages: 500  } 
+                        setPageNumber={(e)=>{ if(e.target.value <= maxPages) setPageNumber(e.target.value); else setPageNumber(pages) }} 
+                    />,
+                    <Movie endpoint={sorted} pageNumber={pageNumber} setMaxPages={setMaxPages} setMaxResult={setMaxResult} limit={limit}/>
                 ]}
             />
             <Block blockNumber="3" customClass="" children={[<Slider title="Vous aimeriez aussi"/>]}/>
@@ -36,11 +52,23 @@ const ElementB2 = (props) => {
             <h2>
                 {props.title}
             </h2>
-            <select name="trie" id="">
-                <option value="">trier</option>
-                <option value="">catégorie 1</option>
-                <option value="">catégorie 2</option>
-            </select>
+            <div className="sort-input">
+                <label>catégorie</label>
+                <select name="trie" id="sortForm">
+                    <option value="popular" onClick={props.action}>Les plus populaires</option>
+                    <option value="nowPlaying" onClick={props.action}>Au cinéma</option>
+                    <option value="topRated" onClick={props.action}>Mieux notées</option>
+                    <option value="upcoming" onClick={props.action}>À venir</option>
+                </select>
+            </div>
+            <div className="sort-input">
+                <label>pages: {props.pageNumber} /{props.maxPages}</label>
+                <input type="number" value={props.pageNumber} onChange={props.setPageNumber}/>
+            </div>
+            <div className="sort-input">
+                <label>Résultats: {props.limit} /{props.maxResult}</label>
+                <input type="number" value={props.limit} onChange={props.setLimit}/>
+            </div>
         </div>
     );
 };
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,33 @@
 import MovieCard from './MovieCard.js';
 import React from 'react';
 import {useState, useEffect} from 'react'
-import {host,getMovieEndpoint,language, API_KEY, options, AjaxRequest,addOneArticleToCart} from '../utilities.js';
-const Movie = () => {
+import {host, getMovieEndpoint,language, API_KEY, options, AjaxRequest,addOneArticleToCart} from '../utilities.js';
+const Movie = (props) => {
     let pageNumber = "page=2"
-    let movieEndPoint = getMovieEndpoint("popular");
-    const limit = 10;
-    const req = `${host.api}/${movieEndPoint}?api_key=${API_KEY}&${language}&${pageNumber}`;
+    // let movieEndPoint = getMovieEndpoint(props.endpoint);
+    // const req = props.request;
+    
+    
     const movies = [];
     const ids = [];
+    
     const [movieState, setMovies] = useState([]);
+    const req = `${host.api}/${getMovieEndpoint(props.endpoint)}?api_key=${API_KEY}&${language}&page=${props.pageNumber}`
     useEffect(()=>{
+      console.log("hello")
+      
       fetch(req)
       .then((res)=>{
           return res.json();
       })
       .then( res =>{
+        // console.log(res.results.length)
+          props.setMaxPages(res.total_pages);
+          props.setMaxResult(res.results.length)
           let i = 0;
           let index = 0;
           for (const element of res.results) {
-            if(i<limit){
+            if(i<props.limit){
              
               ids.push(element.id);
               let movie = {
@@ -42,7 +50,7 @@ const Movie = () => {
           // addMovieFromApi(req,movies,options);
           
         })
-    },[])
+    },[props.endpoint,props.limit,props.pageNumber])
 
     return(
       <div className="movies">
